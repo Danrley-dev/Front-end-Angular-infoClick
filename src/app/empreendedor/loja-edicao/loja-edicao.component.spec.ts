fd891593af056f50ae7f2a54a988587c4a1ac040
FelipeOrtiz-Dev <felipe.ortiz.1610@gmail.com>
2022-07-20 11:00:24 -0300

    [FRONT-END] criação editar loja

---
+++ src/app/empreendedor/loja-edicao/loja-edicao.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LojaEdicaoComponent } from './loja-edicao.component';
+
+describe('LojaEdicaoComponent', () => {
+  let component: LojaEdicaoComponent;
+  let fixture: ComponentFixture<LojaEdicaoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LojaEdicaoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LojaEdicaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
